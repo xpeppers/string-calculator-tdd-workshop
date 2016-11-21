bc7755b31078ae946d4fadfe652cac687c0f5d72
lorisugolini <loris.ugolini@gmail.com>
2016-11-21 19:10:30 +0100

    introduced sqlite and an utility db class

---
+++ integration-test/com/xpeppers/workshop/tdd/DbUtils.java
@@ -0,0 +1,40 @@
+package com.xpeppers.workshop.tdd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbUtils {
+
+	private static Connection connection;
+
+	public static void executeUpdate(String sql) throws SQLException {
+		Connection connection = getConnection();
+		createStatement(connection).executeUpdate(sql);
+	}
+
+	public static ResultSet executeQuery(String sql) throws SQLException {
+		Connection connection = getConnection();
+		return createStatement(connection).executeQuery(sql);
+	}
+
+	public static void closeConnection() throws SQLException {
+		if (connection != null)
+			connection.close();
+	}
+
+	private static Connection getConnection() throws SQLException {
+		if (connection == null || connection.isClosed())
+			connection = DriverManager.getConnection("jdbc:sqlite::memory:");
+
+		return connection;
+	}
+
+	private static Statement createStatement(Connection connection) throws SQLException {
+		Statement statement = connection.createStatement();
+		statement.setQueryTimeout(30);
+		return statement;
+	}
+}
