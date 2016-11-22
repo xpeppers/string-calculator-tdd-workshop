4832b46896e9569eb7d41dbb4ff9580628a35883
lorisugolini <loris.ugolini@gmail.com>
2016-11-22 15:20:47 +0100

    Refactoring: renamed class and methods

--- src/com/xpeppers/workshop/tdd/StringParser.java
+++ src/com/xpeppers/workshop/tdd/StringParser.java
@@ -4,14 +4,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
-public class HeaderParser {
+public class StringParser {
 
 	final private String delimiterAndNumbers;
 	final private List<String> defaultDelimiters;
 	final private boolean isCustomDelimiterSpecified;
 	final private int endOfDelimiterIndex;
 
-	public HeaderParser(String delimiterAndNumbers, String... defaultDelimiters) {
+	public StringParser(String delimiterAndNumbers, String... defaultDelimiters) {
 		this.delimiterAndNumbers = delimiterAndNumbers;
 		this.defaultDelimiters = Arrays.asList(defaultDelimiters);
 
@@ -19,11 +19,11 @@ public class HeaderParser {
 		endOfDelimiterIndex = delimiterAndNumbers.indexOf("\n");
 	}
 
-	public String[] parse() {
-		return allButHeader().split(toRegEx(delimiters()));
+	public String[] extractNumbers() {
+		return allButHeader().split(toOrRegEx(delimiters()));
 	}
 
-	private String toRegEx(List<String> delimiters) {
+	private String toOrRegEx(List<String> delimiters) {
 		return String.join("|", delimiters);
 	}
 
@@ -37,8 +37,8 @@ public class HeaderParser {
 
 	private List<String> detectMulptipleCustomDelimiters() {
 		String delimiters = delimiterAndNumbers.substring(2, endOfDelimiterIndex);
-		List<String> splittedDelimiters = Arrays.asList(delimiters.split("\\]\\["));
-		splittedDelimiters.replaceAll(d -> d.replaceAll("\\[|\\]", ""));
+		List<String> splittedDelimiters = Arrays.asList(delimiters.split(Pattern.quote("]") + Pattern.quote("[")));
+		splittedDelimiters.replaceAll(d -> d.replaceAll(Pattern.quote("[") + "|" + Pattern.quote("]"), ""));
 		splittedDelimiters.replaceAll(d -> Pattern.quote(d));
 		return splittedDelimiters;
 	}
